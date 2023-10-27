f6bdca16a110a8dd1e83fc68efc5a0c71e706fe3
sonuprajapati1 <sonu.prajapati78@gmail.com>
2023-10-27 13:22:20 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const config = {
+    db: {
+        url: 'mongodb+srv://sonuprajapati78:ILOFWeYrDT5ZOKj7@cluster0.vvnqzbn.mongodb.net/',
+        name: 'chatdb'
+    },
+
+    gmail_config: {
+        CLIENT_ID: '344175323902-8ighsuqkkg2hbu6459f9c02ov1v3f0pm.apps.googleusercontent.com',
+        CLIENT_SECRET: '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',
+        REDIRECT_URI: "https://developers.google.com/oauthplayground",
+        REFRESH_TOKEN: ''
+    }
+}
